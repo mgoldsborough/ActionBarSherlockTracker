e1d9c301610a18b9033ffd43cc2c94e3ffb59fd0
Mathew Goldsborough <mgoldsborough@mg2innovations.com>
2012-06-11 08:28:49 -0400

    Initial commit

---
+++ gen/com/google/android/apps/analytics/easytracking/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.android.apps.analytics.easytracking;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
